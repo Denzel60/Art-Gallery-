f0295c7bccd197304954f943db83a4834ddc308a
Denzel <lawrencedenzel60@gmail.com>
2024-07-02 16:09:09 +0300

    "Added react-icons and react-masonry dependencies, updated package-lock.json and package.json, modified App.css, App.jsx, Header.jsx, index.css, and removed Home.jsx"

--- Client-side/src/App.jsx
+++ Client-side/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Home from './pages/Home'
+import Home from './pages/Home/Home.jsx'
 
 function App() {
 
--- Client-side/src/components/Header.jsx
+++ Client-side/src/components/Header.jsx
@@ -1,6 +1,20 @@
+import './components.css'
+import { GiPaintRoller } from "react-icons/gi";
+
 function Header() {
     return (
-        <div>Header</div>
+        <div className="header">
+            <div className="logo">
+                <GiPaintRoller className="logo-icon" size={50} color="black" />
+                <h3 className="logo-text">Art Gallery</h3>
+            </div>
+
+            <div className="navbar">
+                <h3><a href="http://">Trending</a></h3>
+                <h3><a href="http://">Sign In</a></h3>
+                <h3><a href="http://">Sign Up</a></h3>
+            </div>
+        </div>
     )
 }
 
